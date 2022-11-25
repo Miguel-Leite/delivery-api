19877f31cfe33235e800e561e8e99d603faf9be8
Miguel-Leite <miguelleite200leite@email.com>
2022-11-25 10:13:44 +0100

    :fire: :sparkles: remove migrations and update driver database

--- prisma/migrations/20221125054247_create_delivery/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "deliveryman" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "username" TEXT NOT NULL,
-    "password" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "deliveryman_username_key" ON "deliveryman"("username");
--- prisma/migrations/20221125054616_create_client/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "client" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "password" TEXT NOT NULL
-);
