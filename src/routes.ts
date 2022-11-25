5a2ae3b94770809c8e574afeaddf3087f8653927
Miguel-Leite <miguelleite200leite@email.com>
2022-11-25 13:24:40 +0100

    :sparkles: added new router

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/createDeliveries/CreateDeliveryController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 
@@ -13,5 +14,6 @@ routes.post('/deliveryman/authenticate', new AuthenticateDeliverymanController()
 
 routes.post('/client', new CreateClientController().handle);
 routes.post('/deliveryman', new CreateDeliverymanController().handle);
+routes.post('/delivery', new CreateDeliveryController().handle);
 
 export { routes };
